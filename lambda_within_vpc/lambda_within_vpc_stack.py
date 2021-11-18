190863d260672789f374943a4d6845340fc37fa0
wangrong <wangrong@ecs.vuw.ac.nz>
2021-11-18 17:26:16 +1300

    add vpc

--- lambda_within_vpc/lambda_within_vpc_stack.py
+++ lambda_within_vpc/lambda_within_vpc_stack.py
@@ -1,13 +1,7 @@
 from aws_cdk import (
-    core as cdk
-    # aws_sqs as sqs,
+    core as cdk,
+    aws_ec2 as ec2
 )
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
 
 
 class LambdaWithinVpcStack(cdk.Stack):
@@ -15,10 +9,22 @@ class LambdaWithinVpcStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "LambdaWithinVpcQueue",
-        #     visibility_timeout=cdk.Duration.seconds(300),
-        # )
+        vpc = ec2.Vpc(self, 'demoVpc',
+                      cidr="10.0.0.0/16",
+                      max_azs=2,
+                      enable_dns_hostnames=True,
+                      enable_dns_support=True,
+                      subnet_configuration=[
+                          ec2.SubnetConfiguration(
+                              name="Public",
+                              subnet_type=ec2.SubnetType.PUBLIC,
+                              cidr_mask=24
+                          ),
+                          ec2.SubnetConfiguration(
+                              name="Private",
+                              subnet_type=ec2.SubnetType.PRIVATE,
+                              cidr_mask=24
+                          )
+                      ],
+                      nat_gateways=1
+                      )
